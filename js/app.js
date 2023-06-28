f67ca0c663d9033f31169559f3f571761fcc7570
peluboy <imulep2104@gmail.com>
2023-06-28 18:57:06 +0100

    finished up the responsiveness

--- js/app.js
+++ js/app.js
@@ -3,7 +3,7 @@ const sidebar = document.querySelector(".sidebar");
 
 sidebarToggle.addEventListener("click", () => {
   sidebar.classList.toggle("sidebar-open");
-  sidebarToggle.classList.toggle("sidebar-toggle-open"); // Add this line
+  sidebarToggle.classList.toggle("sidebar-toggle-open");
 });
 
 document.addEventListener("click", (event) => {
@@ -12,17 +12,6 @@ document.addEventListener("click", (event) => {
 
   if (!isInsideSidebar && !isInsideToggle) {
     sidebar.classList.remove("sidebar-open");
-    sidebarToggle.classList.remove("sidebar-toggle-open"); // Add this line
+    sidebarToggle.classList.remove("sidebar-toggle-open");
   }
 });
-
-// document.addEventListener("click", (event) => {
-//   // Check if the clicked element is inside the sidebar or sidebar toggle button
-//   const isInsideSidebar = sidebar.contains(event.target);
-//   const isInsideToggle = sidebarToggle.contains(event.target);
-
-//   // If the clicked element is outside the sidebar and sidebar toggle button, close the sidebar
-//   if (!isInsideSidebar && !isInsideToggle) {
-//     sidebar.classList.remove("sidebar-open");
-//   }
-// });
